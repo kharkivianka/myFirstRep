df5a193d3c24bdc6abf4c30ae74490abcfbb3767
Yana Onyshchenko <>
2018-10-21 16:02:43 +0300

    first initial commit with empty project

---
+++ RemoteGitHubProject/RemoteGitHubProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemoteGitHubProject
+//
+//  Created by Yana Onyshchenko on 10/21/18.
+//  Copyright © 2018 Yana Onyshchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
